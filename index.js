76113e256d71cb938780ca684b3bef4a77c179a2
Vageesha G <vageeshag75@gmail.com>
2025-01-15 07:11:09 +0000

    Now it can dynamically create div and allocate for having desired div

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector("#myRange");
 const output = document.querySelector("#demo");
-const clear = document.querySelector(".reset")
+const clear = document.querySelector(".reset");
+const container = document.querySelector(".container");
 output.innerHTML = `${slider.value } X ${slider.value}`;
 
 
@@ -10,11 +11,38 @@ output.innerHTML = `${slider.value } X ${slider.value}`;
 
 slider.addEventListener("input", (e) => {
     output.innerHTML = `${slider.value } X ${slider.value}`;
+    let gridDimension = slider.value;
+    createGrid(gridDimension);
 });
 
 clear.addEventListener("click", (e) => {
     slider.value = 4;
     console.log(e);
     output.innerHTML = `${slider.value } X ${slider.value}`;
-})
+    createGrid (4);
+});
+
+function createGrid (gridDimension) {
+    container.innerHTML = "";
+    const width = 480/gridDimension;
+    const height = 480/gridDimension;
+    
+    for (let i = 0; i < gridDimension; i++) { // Outer loop for rows
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+    
+        for (let j = 0; j < gridDimension; j++) { // Inner loop for columns
+            const colDiv = document.createElement("div");
+            colDiv.classList.add("col");
+            colDiv.style.width = `${width}px`;
+            colDiv.style.height = `${height}px`;
+            rowDiv.appendChild(colDiv);
+        }
+
+        container.appendChild(rowDiv);
+    }
+
+
+
+}
 
